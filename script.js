7132dcea461cb23df9717cdb88eeda6ebd2a1c59
Burke Mamlin <burke@openmrs.org>
2017-06-27 21:25:46 -0400

    code cleanup

--- script.js
+++ script.js
@@ -5,29 +5,71 @@ var lastKeepAliveTime = 0;
 var cycle = 0;
 
 if (!('webkitSpeechRecognition' in window)) {
-    //Speech API not supported here
     incompatible();
     recognition = {};
-} else { //Let’s do some cool stuff :)
-    var recognition = new webkitSpeechRecognition(); //That is the object that will manage our whole recognition process. 
-    recognition.continuous = true;   //Suitable for dictation. 
-    recognition.interimResults = true;  //If we want to start receiving results even if they are not final.
-    //Define some more additional parameters for the recognition:
+} else {
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
     recognition.lang = "en-US"; 
-    recognition.maxAlternatives = 1; //Since from our experience, the highest result is really the best...
+    recognition.maxAlternatives = 1;
     keepAlive();
 }
 
+var commands = [
+    {
+        "when": /^(clear|clear screen)$/i,
+        "do": function(e) { clearScreen(); }
+    },
+    {
+        "when": /^stop listening$/i,
+        "do": function(e) {
+            $(':last', '#out').remove();
+            startButton();
+        }
+    },
+    {
+        "when": /^\n\n+$/,
+        "do": function(e) {
+            $(':last', '#out').remove();
+            $('#out').append('<div class="output paragraph"></div>');
+        }
+    },
+    {
+        "when": /^\n$/,
+        "do": function(e) {
+            $(':last', '#out').remove();
+            $('#out').append('<div class="output newline"></div>');
+        }
+    },
+    {
+        "when": /^(zoom in|bigger)$/i,
+        "do": function(e) {
+            $(':last', '#out').remove();
+            setTimeout(increaseFont, 10);
+        }
+    },
+    {
+        "when": /^(zoom out|smaller)$/i,
+        "do": function(e) {
+            $(':last', '#out').remove();
+            setTimeout(decreaseFont, 10);
+        }
+    },
+    {
+        "when": /^(undo|delete)$/i,
+        "do": function(e) {
+            $(':nth-last-child(-n+2)', '#out').remove();
+        }
+    }
+    ];
+
 recognition.onstart = function() {
-    //Listening (capturing voice from audio input) started.
-    //This is a good place to give the user visual feedback about that (i.e. flash a red light, etc.)
-    console.log("listening");
     cycle++;
     listening = true;
 };
 
 recognition.onend = function() {
-    //Again – give the user feedback that you are not listening anymore. If you wish to achieve continuous recognition – you can write a script to start the recognizer again here.
     if (autoRestart) {
         recognition.start();
     } else {
@@ -37,60 +79,30 @@ recognition.onend = function() {
     }
 };
 
-recognition.onresult = function(event) { //the event holds the results
-//Yay – we have results! Let’s check if they are defined and if final or not:
-    if (typeof(event.results) === 'undefined') { //Something is wrong…
+recognition.onresult = function(event) {
+    if (typeof(event.results) === 'undefined') {
         recognition.stop();
         return;
     }
-
     var out = '';
     var isFinal = true;
     for (var i = event.resultIndex; i < event.results.length; i++) {
         if (!event.results[i].isFinal) isFinal = false;
         out = out + event.results[i][0].transcript + ' ';
     }
-    out = out.replace(/^ +| +$/g,''); // remove spaces (not newlines)
+    out = out.replace(/^ +| +$/g,''); // trim, preserving newlines
     if (isFinal) {
-        if (/^(clear|clear screen)$/i.test(out)) {
-            clearScreen();
-            return;
-        }
-        if (/^stop listening$/i.test(out)) {
-            $(':last', '#out').remove();
-            startButton();
-            return;
-        }
-        if (/^\n\n+$/.test(out)) {
-            $(':last', '#out').remove();
-            $('#out').append('<div class="output paragraph"></div>');
-            return;
-        }
-        if (/^\n$/.test(out)) {
-            $(':last', '#out').remove();
-            $('#out').append('<div class="output newline"></div>');
-            return;
-        }
-        if (/^(zoom in|bigger)$/i.test(out)) {
-            $(':last', '#out').remove();
-            setTimeout(increaseFont, 10);
-            return;
-        }
-        if (/^(zoom out|smaller)$/i.test(out)) {
-            $(':last', '#out').remove();
-            setTimeout(decreaseFont, 10);
-            return;
-        }
-        if (/^(undo|delete)$/i.test(out)) {
-            $(':nth-last-child(-n+2)', '#out').remove();
-            return;
+        for (var i=0; i < commands.length; i++) {
+            if (commands[i].when.test(out)) {
+                commands[i].do(event);
+                return;
+            }
         }
     }
     var elemId = 'i' + cycle + '-' + event.resultIndex;
     var elem = $('#' + elemId);
     if (!elem.length) {
         elem = $('<span id="'+elemId+'" class="output"></span>');
-        //console.log(div);
         $('#out').append(elem);
     }
     if (isFinal) {
@@ -104,24 +116,7 @@ recognition.onresult = function(event) { //the event holds the results
     $('html, body').animate({
         scrollTop: $(document).height()
     }, 'fast');
-}; 
-
-/*
-recognition.onerror = function(event) {
-    console.log("error event!");
-    console.log(event);
 };
-
-recognition.onspeechend = function(event) {
-    console.log("speech end");
-    console.log(event);
-}
-
-recognition.onsaudioend = function(event) {
-    console.log("audio end");
-    console.log(event);
-}
-*/
 
 function keepAlive() {
     recognition.stop();
